bb66bf03edf0483d2925b976c49a12caeb37f299
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-20 23:04:12 +0530

    HOME

---
+++ long.java
@@ -0,0 +1,27 @@
+class am {
+      public static void main (String [] args ){
+      byte a;
+      a=70;
+      System.out.println (a);
+
+      short Bankbalance;
+      Bankbalance=32222;
+      System.out.println (Bankbalance);
+
+      int h=7;
+      h=7;
+      System.out.println (h);
+
+      long t=12349432111111l;
+      System.out.println (t);
+
+float d= 7.1233333333;
+System.out.println (d);
+
+double j=8.1233477424;
+System.out.println (j);
+
+
+}
+}
+
